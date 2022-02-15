90d34807442dcb44664372719c29ec9ceeffeb6e
filipculig <filip.culig@porsche.digital>
2022-02-15 10:02:50 +0100

    Initial Commit

---
+++ edu-app/edu_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  edu_appApp.swift
+//  edu-app
+//
+//  Created by Filip Culig on 15.02.2022..
+//
+
+import SwiftUI
+
+@main
+struct edu_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ edu-appUITests/edu_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  edu_appUITestsLaunchTests.swift
+//  edu-appUITests
+//
+//  Created by Filip Culig on 15.02.2022..
+//
+
+import XCTest
+
+class edu_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
