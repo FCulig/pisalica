474cc33a6dea76e6222d4f05f05dbf21e6c74446
Filip Culig <filip.culig@gmail.com>
2023-09-22 18:27:57 +0200

    Updated configuration

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -3,12 +3,10 @@
 set -eo pipefail
 
 #cd ../../
-
-echo "$PWD"
+# -configuration AppStoreDistribution \
 
 xcodebuild -workspace edu-app.xcworkspace \
            -scheme edu-app\
            -sdk iphoneos \
-           -configuration AppStoreDistribution \
            -archivePath $PWD/build/edu-app.xcarchive \
            clean archive | xcpretty
