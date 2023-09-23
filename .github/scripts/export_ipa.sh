e9fd63f0222d6eb35a60e2bfd33a76ea27c1f12a
Filip Culig <filip.culig@gmail.com>
2023-09-23 12:56:25 +0200

    Updated export script

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -1,7 +1,7 @@
 set -eo pipefail
 
 xcodebuild -archivePath $PWD/build/edu-app.xcarchive \
-           -exportOptionsPlist edu-app/edu-app/exportOptions.plist \
+           -exportOptionsPlist edu-app/ExportOptions.plist \
            -exportPath $PWD/build \
            -allowProvisioningUpdates \
            -exportArchive | xcpretty
