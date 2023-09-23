073dfab10ff4356866ae6c98b88d158f254bb7dd
Filip Culig <filip.culig@gmail.com>
2023-09-23 13:35:49 +0200

    Updated app name

--- .github/scripts/publish_testflight.sh
+++ .github/scripts/publish_testflight.sh
@@ -1,3 +1,3 @@
 set -eo pipefail
 
-xcrun altool --upload-app -t ios -f build/Calculator\ iOS.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
+xcrun altool --upload-app -t ios -f build/Pisanka.ipa -u "$APPLEID_USERNAME" -p "$APPLEID_PASSWORD" --verbose
