9a6dc460edf84e3d9a0ec76bff83d83677e4a6f6
Filip Culig <filip.culig@gmail.com>
2024-02-03 12:57:28 +0100

    XML reports

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 xcodebuild -workspace edu-app.xcworkspace \
            -destination platform=iOS\ Simulator,OS=16.2,name=iPhone\ 14,arch=x86_64 \
            -scheme edu-app \
-            clean test | xcpretty --report html
+            clean test | xcpretty --report junit
