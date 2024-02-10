51fa134d9d78070db2ad4f3a4485e42a9077647f
Filip Culig <filip.culig@gmail.com>
2024-02-10 21:30:46 +0100

    Refactored ShopView to use Screen

--- edu-app/Screens/Shop/ShopView.swift
+++ edu-app/Screens/Shop/ShopView.swift
@@ -23,70 +23,29 @@ struct ShopView: View {
     // MARK: - View components -
 
     var body: some View {
-        if isTablet {
-            foregroundContent
-                .background(
-                    AppImage.houseBackgroundTabletImage.image
-                        .scaledToFill()
-                        .ignoresSafeArea()
-                        .offset(x: 80, y: 0)
-                        .blur(radius: 3)
-                )
-                .navigationBarHidden(true)
-        } else {
-            foregroundContent
-                .background(
-                    AppImage.houseBackgroundImage.image
-                        .aspectRatio(contentMode: .fill)
-                        .ignoresSafeArea()
-                        .blur(radius: 3)
-                )
-                .navigationBarHidden(true)
-                .edgesIgnoringSafeArea(.bottom)
-        }
-    }
-
-    var foregroundContent: some View {
-        VStack {
-            HStack {
-                backButton
-                
-                Spacer()
-                
-                coinsBalance
-            }
-            
-            Spacer()
-            
-            shopItemsPanel
-            
-            Spacer()
-        }
-    }
-
-    var backButton: some View {
-        Button(action: {
-                   dismiss()
-               },
-               image: AppImage.previousButton.image)
-            .frame(height: 70, alignment: .top)
-            .padding(.top, 15)
-            .padding(.leading, isTablet ? 15 : 0)
+        Screen(shouldShowBackButton: true,
+               centerContent: AnyView(shopItemsPanel),
+               topRightContent: AnyView(coinsBalance))
+        .ignoresSafeArea(edges: isTablet ? .all : [.top, .trailing, .bottom])
     }
 
     var coinsBalance: some View {
-        ZStack {
-            AppImage.coinsBalanceBackground.image
-                .scaledToFit()
-                .frame(height: 65)
-            Text("\(viewModel.shopService.balance)")
-                .foregroundColor(.white)
-                .padding(.leading, 45)
-                .padding(.bottom, 5)
-                .font(.system(size: 25).weight(.bold))
+        VStack(spacing: 0) {
+            ZStack {
+                AppImage.coinsBalanceBackground.image
+                    .scaledToFit()
+                    .frame(height: 65)
+                Text("\(viewModel.shopService.balance)")
+                    .foregroundColor(.white)
+                    .padding(.leading, 45)
+                    .padding(.bottom, 5)
+                    .font(.system(size: 25).weight(.bold))
+            }
+            .padding(.top, 15)
+            .padding(.trailing, isTablet ? 15 : 0)
+            
+            Spacer()
         }
-        .padding(.top, 15)
-        .padding(.trailing, isTablet ? 15 : 0)
     }
 
     var shopItemsPanel: some View {
@@ -119,7 +78,8 @@ struct ShopView: View {
                 }
             }
         }
-        .padding()
+        .padding(.vertical, isTablet ? 100 : 0)
+        .padding(.trailing, isTablet ? 0 : 55)
         .onLoad { viewModel.getShopItems() }
     }
 }
